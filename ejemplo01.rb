d1eeb338a7352aedd7fbc53129a845d993c6d5ec
lizbet <lizbethestrellagranizo@gmail.com>
2016-04-14 13:28:27 -0500

    primer archivo:ejemplo01.rb

---
+++ ejemplo01.rb
@@ -0,0 +1,6 @@
+cantantes=["enrique iglesias","nicky jam","ruby","j alvares",
+	"tony dize,""maluma","rio roma","arcangel",
+	"paulina rubio","mark anthony"]
+	 puts"mi cantante favorito es:"+ cantantes[8]
+	
+	gets()
